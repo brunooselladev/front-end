f85f930ad79ddba03b3942fbbc039f569fd2e7c5
brunooselladev <brunooselladev@gmail.com>
2026-02-19 04:16:06 -0300

    no proxy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-  target: 'https://mappa.marbrus.com.ar',
-  changeOrigin: true,
-  configure: (proxy) => {
-    proxy.on('proxyReq', (proxyReq) => {
-      console.log('Forwarding to:', proxyReq.host + proxyReq.path);
-    });
-  },
-},
+        target: 'https://mappa.marbrus.com.ar',
+        changeOrigin: true
+      },
     },
   },
 })
